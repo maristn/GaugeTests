a3a75447daa10be80eb32a29145bfdc60b7ce428
maristn <marianasantana@gmail.com>
2016-05-19 09:55:08 -0300

    teste

---
+++ Gemfile
@@ -0,0 +1,3 @@
+source "https://rubygems.org"
+
+gem 'gauge-ruby', '~>0.2.0', :group => [:development, :test]
---
+++ step_implementations/step_implementation.rb
@@ -0,0 +1,9 @@
+
+step 'Vowels in English language are <vowels>.' do |x|
+end
+
+step 'The word <word> has <count> vowels.' do |x, y|
+end
+
+step 'Almost all words have vowels <table>' do |x|
+end
